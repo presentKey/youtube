b531ffb7f2f2291f5fa2aa41a29e5206644e0c24
presentKey <kim.hyeonjae4@gmail.com>
2023-01-03 22:12:54 +0900

    new: Calculate published time

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function VideoCard({ video }) {
+  const time = timeForToday(video.snippet.publishedAt);
   return (
     <div>
       <img
@@ -9,7 +10,32 @@ export default function VideoCard({ video }) {
       />
       <span>{video.snippet.title}</span>
       <span>{video.snippet.channelTitle}</span>
-      <span>{video.snippet.publishedAt}</span>
+      <span>{time}</span>
     </div>
   );
+}
+
+function timeForToday(value) {
+  const today = new Date();
+  const timeValue = new Date(value);
+
+  const betweenTime = Math.floor(
+    (today.getTime() - timeValue.getTime()) / 1000 / 60
+  );
+  if (betweenTime < 1) return 'just before';
+  if (betweenTime < 60) {
+    return `${betweenTime} minute ago`;
+  }
+
+  const betweenTimeHour = Math.floor(betweenTime / 60);
+  if (betweenTimeHour < 24) {
+    return `${betweenTimeHour} hours ago`;
+  }
+
+  const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+  if (betweenTimeDay < 365) {
+    return `${betweenTimeDay} day ago`;
+  }
+
+  return `${Math.floor(betweenTimeDay / 365)} year ago`;
 }
