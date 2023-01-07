e0449bee74a3957e56375a29f034f4b239d9c854
presentKey <kim.hyeonjae4@gmail.com>
2023-01-07 21:42:25 +0900

    new: 검색 구현

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import VideoCard from '../components/VideoCard/VideoCard';
 
 export default function Search() {
   const { keyword } = useParams();
-  return <div>Search {keyword}</div>;
+  const {
+    isLoading,
+    error,
+    data: search,
+  } = useQuery(
+    ['related'],
+    async () => {
+      return fetch('/data/keyword.json').then((res) => res.json());
+    },
+    { staleTime: 1000 * 60 * 5 }
+  );
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+  return (
+    <div>
+      <ul>
+        {search.items.map((video) => (
+          <li key={video.id}>
+            <VideoCard video={video} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
