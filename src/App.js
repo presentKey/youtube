093c9efca0b8037977244cb4fb6927e2927c44ba
presentKey <kim.hyeonjae4@gmail.com>
2023-01-08 16:58:04 +0900

    refactor: 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import './App.css';
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Root from './pages/Root';
-import Search from './pages/Search';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import Watch from './pages/Watch';
-
-const queryClient = new QueryClient();
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: '/videos/:keyword', element: <Search /> },
-      { path: '/videos/watch/:id', element: <Watch /> },
-    ],
-  },
-]);
+import { Outlet } from 'react-router-dom';
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={true} />
-    </QueryClientProvider>
+    <>
+      <Outlet />
+    </>
   );
 }
